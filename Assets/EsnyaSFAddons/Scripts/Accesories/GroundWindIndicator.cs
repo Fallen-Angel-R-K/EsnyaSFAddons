fa0bba7d702f1e5fc12b7ee9b668f322c675203c
esnya <esnya@nekometer.com>
2022-03-24 15:51:15 +0900

    :boom: feat!: Replace scripts from original
    BREAKING CHANGE: Most of GUID changed

--- Assets/EsnyaSFAddons/Scripts/Accesories/GroundWindIndicator.cs
+++ Assets/EsnyaSFAddons/Scripts/Accesories/GroundWindIndicator.cs
@@ -1,8 +1,6 @@
 using TMPro;
 using UdonSharp;
 using UnityEngine;
-using VRC.SDKBase;
-using VRC.Udon;
 
 namespace EsnyaSFAddons
 {
@@ -14,7 +12,6 @@ namespace EsnyaSFAddons
         [UdonSharpComponentInject] public SAV_WindChanger windChanger;
         public Transform directionIndicator;
         public TextMeshProUGUI speedText;
-
 
         private void OnEnable()
         {
