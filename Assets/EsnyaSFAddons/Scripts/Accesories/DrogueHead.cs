02940e3085440e369f5127fbf784238ea758a11c
esnya <esnya@nekometer.com>
2022-03-25 10:57:47 +0900

    :sparkles: feat(dfunc): BuddyPod

---
+++ Assets/EsnyaSFAddons/Scripts/Accesories/DrogueHead.cs
@@ -0,0 +1,32 @@
+﻿using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+
+namespace EsnyaSFAddons
+{
+    [RequireComponent(typeof(Rigidbody))]
+    [UdonBehaviourSyncMode(BehaviourSyncMode.None)]
+    public class DrogueHead : UdonSharpBehaviour
+    {
+        public string prefix = "Probe";
+        public UdonSharpBehaviour eventTarget;
+        public string eventName = "_Contact";
+        [System.NonSerialized] public SaccEntity targetEntity;
+        [System.NonSerialized] public Collider probeCollider;
+
+        private void OnTriggerEnter(Collider other)
+        {
+            if (!other || !other.attachedRigidbody || !other.gameObject.name.StartsWith(prefix)) return;
+            probeCollider = other;
+
+            var vehicleRigidbody = other.attachedRigidbody;
+            if (!Networking.IsOwner(vehicleRigidbody.gameObject)) return;
+
+            targetEntity= vehicleRigidbody.GetComponent<SaccEntity>();
+            if (!targetEntity) return;
+
+            eventTarget.SendCustomEvent(eventName);
+        }
+    }
+}
